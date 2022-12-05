8c7923d8b83e239d48f5b8a8a5eb2e2ce00cad78
Jiripoz <103076776+Jiripoz@users.noreply.github.com>
2022-12-05 14:32:48 -0300

    solutions to day 3, 4 and 5

---
+++ output_day3.py
@@ -0,0 +1,77 @@
+rucksack = open('input_day3.txt', 'r').readlines()
+
+map1={
+    'a':1,
+    'b':2,
+    'c':3,
+    'd':4,
+    'e':5,
+    'f':6,
+    'g':7,
+    'h':8,
+    'i':9,
+    'j':10,
+    'k':11,
+    'l':12,
+    'm':13,
+    'n':14,
+    'o':15,
+    'p':16,
+    'q':17,
+    'r':18,
+    's':19,
+    't':20,
+    'u':21,
+    'v':22,
+    'w':23,
+    'x':24,
+    'y':25,
+    'z':26,
+    'A':27,
+    'B':28,
+    'C':29,
+    'D':30,
+    'E':31,
+    'F':32,
+    'G':33,
+    'H':34,
+    'I':35,
+    'J':36,
+    'K':37,
+    'L':38,
+    'M':39,
+    'N':40,
+    'O':41,
+    'P':42,
+    'Q':43,
+    'R':44,
+    'S':45,
+    'T':46,
+    'U':47,
+    'V':48,
+    'W':49,
+    'X':50,
+    'Y':51,
+    'Z':52
+}
+
+final=0
+for i in range(0, len(rucksack), 3):
+    print(i)
+    line1=rucksack[i]
+    line2=rucksack[i+1]
+    print(line2)
+    line3=rucksack[i+2]
+    print(line3)
+    lcharacter=[]
+
+    for character in line1:
+        if character in line2:
+            if character in line3:
+                lcharacter.append(character)
+
+    print(lcharacter)
+    print(map1[str(lcharacter[0])])
+    final=final+map1[str(lcharacter[0])]
+
+print(final)
